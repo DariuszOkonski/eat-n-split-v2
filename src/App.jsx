27d4527f8e370b955c33a6f2a0098e2397ffadd8
Dariusz Okoński <darek200180@gmail.com>
2025-03-27 10:24:08 +0100

    initialization

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import FriendsList from './FriendsList';
+import FormAddFriend from './FormAddFriend';
+import Button from './Button';
+import FormSplitBill from './FormSplitBill';
+
+// const initialFriends = [
+//   {
+//     id: 118836,
+//     name: 'Clark',
+//     image: 'https://i.pravatar.cc/48?u=118836',
+//     balance: -7,
+//   },
+//   {
+//     id: 933372,
+//     name: 'Sarah',
+//     image: 'https://i.pravatar.cc/48?u=933372',
+//     balance: 20,
+//   },
+//   {
+//     id: 499476,
+//     name: 'Anthony',
+//     image: 'https://i.pravatar.cc/48?u=499476',
+//     balance: 0,
+//   },
+// ];
+
+function App() {
+  return (
+    <div className='app'>
+      <div className='sidebar'>
+        <FriendsList />
+        <FormAddFriend />
+        <Button>Add friend</Button>
+      </div>
+      <FormSplitBill />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FormSplitBill.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Button from './Button';
+
+function FormSplitBill() {
+  return (
+    <form className='form-split-bill'>
+      <h2>Split a bill with X</h2>
+
+      <label>🤑 Bill value</label>
+      <input type='text' />
+
+      <label>🙅 Your expense</label>
+      <input type='text' />
+
+      <label>🤼 X's expense</label>
+      <input type='text' disabled />
+
+      <label>💵 Who is paying the bill</label>
+      <select>
+        <option value='user'>You</option>
+        <option value='friend'>X</option>
+      </select>
+
+      <Button>Split bill</Button>
+    </form>
+  );
+}
+
+export default FormSplitBill;
