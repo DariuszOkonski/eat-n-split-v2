fe1c3fc9e5c1035f642b5aa37f0c161849a2bf60
Dariusz Okoński <darek200180@gmail.com>
2025-04-01 10:55:04 +0200

    add handleSubmit

--- src/FormSplitBill.jsx
+++ src/FormSplitBill.jsx
@@ -1,14 +1,24 @@
 import React, { useState } from 'react';
 import Button from './Button';
 
-function FormSplitBill({ selectedFriend }) {
+function FormSplitBill({ selectedFriend, onSplitBill }) {
   const [bill, setBill] = useState(0);
   const [paidByUser, setPaidByUser] = useState(0);
   const paidByFriend = bill ? bill - paidByUser : 0;
   const [whoIsPaying, setWhoIsPaying] = useState('user');
 
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!bill || !paidByUser) {
+      return;
+    }
+
+    onSplitBill(whoIsPaying === 'user' ? paidByFriend : -paidByUser);
+  }
+
   return (
-    <form className='form-split-bill'>
+    <form className='form-split-bill' onSubmit={handleSubmit}>
       <h2>Split a bill with {selectedFriend.name}</h2>
 
       <label>🤑 Bill value</label>
